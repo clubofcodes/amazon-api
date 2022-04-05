18b0e73c03e8d47ffd22af8a354fe1817db35161
Rahul Jagetiya <63360644+clubofcodes@users.noreply.github.com>
2022-04-05 23:52:56 +0530

    Setup of amazon clone project's API.

--- index.js
+++ index.js
@@ -1,8 +1,32 @@
+// const express = require("express");
+// const app = express();
+// const product = require("./api/product");
+
+// app.use(express.json({ extended: false }));
+// app.use("/api", product);
+
+// app.listen(process.env.PORT || 3575, () => console.log(`Server is running on port number ${process.env.PORT || 3575}`));
+
+require("dotenv").config();
 const express = require("express");
 const app = express();
-const product = require("./api/product");
+const mongoose = require("mongoose");
+require("./Database/Connect");
+const cors = require("cors");
+const router = require("./routes/router");
+const cookieParser = require("cookie-parser");
+
+const Products = require("./Models/productSchema");
+
+const DefaultData = require("./defaultData");
 
 app.use(express.json({ extended: false }));
-app.use("/api", product);
+app.use(cookieParser(""));
+app.use(cors());
+app.use("/api", router);
 
-app.listen(process.env.PORT || 3575, () => console.log(`Server is running on port number ${process.env.PORT || 3575}`));
+app.listen(process.env.PORT || 3575, () => {
+    console.log(`Server is running on port number ${process.env.PORT || 3575}`);
+});
+
+DefaultData();
